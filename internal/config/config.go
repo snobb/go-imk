4882e4f447d33cb94c103e7ae078dd7c71004463
Aleksei Kozadaev <578528+snobb@users.noreply.github.com>
2025-12-25 18:00:57 +0000

    config: update usage and add some constraints.

--- internal/config/config.go
+++ internal/config/config.go
@@ -48,19 +48,19 @@ func (c *Config) ParseCmdArgs() error {
 		"if a directory is supplied, add all its sub-directories as well.")
 
 	pflag.BoolVarP(&c.OneRun, "once", "o", false,
-		"run command once and exit on event.")
+		"run primary command once and exit on event.")
 
 	pflag.BoolVarP(&c.RunNow, "immediate", "i", false,
-		"run command immediately before watching for events.")
+		"run commands immediately before watching for events.")
 
 	pflag.StringVarP(&c.PrimaryCmd, "command", "c", "",
-		"command to execute when file is modified.")
+		"primary command to execute when a file or a folder is modified.")
 
 	pflag.StringVarP(&c.SecondaryCmd, "run", "u", "",
-		"command to execute if primary command succeeded - runs in background. ")
+		"secondary command to execute if primary command succeeded - runs in background.")
 
 	pflag.DurationVarP(&c.TearDownTimeout, "timeout", "k", 0,
-		"timeout after which to kill the command subproces (default - do not kill).")
+		"timeout after which to kill the command subprocess (default - do not kill).")
 
 	pflag.Usage = usage
 
@@ -78,6 +78,10 @@ func (c *Config) ParseCmdArgs() error {
 
 	if c.PrimaryCmd == "" && c.SecondaryCmd == "" {
 		return fmt.Errorf("either primary or secondary command must be specified")
+	}
+
+	if c.OneRun && c.SecondaryCmd != "" {
+		return fmt.Errorf("secondary command is not supported with -o flag")
 	}
 
 	c.Files = pflag.Args()
@@ -110,11 +114,12 @@ func (c *Config) EnrichFiles() error {
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 	pflag.PrintDefaults()
-	fmt.Println("\nThe secondary command will run in the background and will be " +
-		"killed if the primary command fails.")
+	fmt.Println("\nIt is required to specify either primary or secondary command (or both).")
+	fmt.Println("\nThe secondary command will run in the background and will be restarted " +
+		"immediately after the primary command is executed the next time.")
 	fmt.Println("\nExamples:")
 	fmt.Println("  imk -rc 'go build ./...' src/")
 	fmt.Println("  imk -rc 'go build ./...' src/ -k 5m")
-	fmt.Println("  imk -ric 'go build ./...' -u 'go run ./...' src/ -o")
+	fmt.Println("  imk -ric 'go build ./...' -u 'go run ./...' src/")
 	fmt.Println()
 }
